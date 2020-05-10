a4de1a1c91a4e25cc045e697a7252838fb4d0e66
Eri Nagashima <es6.ukf@gmail.com>
2020-05-10 21:40:18 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// モーダル要素の取得
+const modalElem = document.getElementById('easyModal');
+
+// 開くボタン取得
+const modalOpenBtn = document.getElementById('modalOpen');
+
+// 閉じるボタン取得
+const modalCloseBtn = document.querySelector('.modalClose');
+
+// クリックで開くイベントリスナー
+modalOpenBtn.addEventListener('click', modalOpen);
+
+// クリックで閉じるイベントリスナー
+modalCloseBtn.addEventListener('click', modalClose);
+
+// モーダル以外の場所クリックのイベントリスナー
+const modal = document.querySelector('.modal');
+
+// モーダルオープンの関数
+function modalOpen() {
+  modalElem.style.display = 'block';
+}
+
+// モーダルを閉じる関数
+function modalClose() {
+  modalElem.style.display = 'none';
+  modalCloseBtn.style.cursor = 'pointer';
+}
+
+// モーダル以外がクリックされた時に閉じる関数
+function modalCloseOutside() {
+  modalElem.style.display = 'none';
+}
